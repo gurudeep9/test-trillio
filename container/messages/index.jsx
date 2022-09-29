2c4aa0a45189117d5c8e1d17921ea4c59387ac0f
Jesus Juvinao <juvinaojesusd@gmail.com>
2022-09-29 04:14:44 -0500

    fix: erros code smells

--- container/messages/index.jsx
+++ container/messages/index.jsx
@@ -1,172 +1,175 @@
-import { useLazyQuery, useSubscription, useMutation, useQuery } from '@apollo/client'
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable consistent-return */
+import {
+  useLazyQuery,
+  useSubscription,
+  useMutation,
+  useQuery
+} from '@apollo/client'
 import { useUser } from 'components/hooks/useUser'
 import { MessageComp } from 'components/Messages'
 import { ContainerContextMessage } from 'components/Messages/styled'
-import { GET_USER } from 'container/profile/queries'
-// import { Context } from 'Context'
-import { GET_MESSAGES, NEW_MESSAGE, NEW_STORE, SEND_MESSAGES } from 'gql/Messages'
+import { } from 'container/profile/queries'
+import {
+  GET_MESSAGES,
+  NEW_MESSAGE,
+  SEND_MESSAGES
+} from 'gql/Messages'
 import React, { useContext, useEffect, useRef, useState } from 'react'
-import { validationSubmitHooks } from 'utils'
 import { Context } from '../../context/index'
 import { GET_ALL_STORY_ACTIVE_MESSAGE_ORDER } from './queries'
-import { useApolloClient } from '@apollo/client'
+
 
 export const Messages = () => {
-    //ESTADOS
-    const { setAlertBox, selectedStore, setStoreChatActive, hidden } = useContext(Context)
-    const [show, setShow] = useState(false)
-    const [dataUser, { loading: loUser }] = useUser()
-    const { id } = dataUser || {}
-    const [values, setValues] = useState({})
+  // ESTADOS
+  const { setAlertBox, selectedStore, setStoreChatActive, hidden } = useContext(Context)
+  const [show, setShow] = useState(false)
+  const [dataUser] = useUser()
+  const { id } = dataUser || {}
+  const [values, setValues] = useState({})
 
-    const [errors, setErrors] = useState({})
-    const [search, setSearch] = useState('')
-    const handleChangeFilter = e => {
-        setSearch(e.target.value)
+  const [errors, setErrors] = useState({})
+  const [search, setSearch] = useState('')
+  const handleChangeFilter = e => {
+    setSearch(e.target.value)
+  }
+  const handleChange = (e, error) => {
+    setValues({ ...values, [e.target.name]: e.target.value })
+    setErrors({ ...errors, [e.target.name]: error })
+  }
+  // QUERIES
+
+
+  const [getMessages, { data: messageData }] = useLazyQuery(GET_MESSAGES, {
+    context: { clientName: 'admin-server' },
+    fetchPolicy: 'network-only',
+    onError: err => {
+      return setAlertBox({
+        message: `${err}`,
+        duration: 10000,
+        color: 'warning'
+      })
     }
-    const handleChange = (e, error) => {
-        setValues({ ...values, [e.target.name]: e.target.value })
-        setErrors({ ...errors, [e.target.name]: error })
+  })
+  const { data: dataStoreActiveOrder } = useQuery(GET_ALL_STORY_ACTIVE_MESSAGE_ORDER, {
+    fetchPolicy: 'network-only',
+    onError: () => {
+      return {}
     }
-    // QUERIES
-
-
-    const [getMessages, { data: messageData, refetch, fetchMore }] = useLazyQuery(GET_MESSAGES, {
-        context: { clientName: "admin-server" },
-        fetchPolicy: 'network-only',
-        onError: err => setAlertBox({
-            message: `${err}`,
-            duration: 10000,
-            color: 'warning'
-        })
-    })
-    const { data: dataStoreActiveOrder } = useQuery(GET_ALL_STORY_ACTIVE_MESSAGE_ORDER, {
-        fetchPolicy: 'network-only',
-        onError: err => console.log({
-            message: `${err}`,
-        })
-    })
-    const client = useApolloClient()
-    // console.log(client)
-    const { data: messageDataNew, error: messageError } = useSubscription(NEW_MESSAGE, {
-        pollInterval: 10,
-        onSubscriptionComplete: () => {
-            // const dataMessage = client.readQuery({ query: GET_MESSAGES })
-        },
-        onSubscriptionData: ({ subscriptionData }) => {
-            // client.writeQuery({
-            //     query: GET_MESSAGES,
-            //     // data: {
-            //     //     ...messageData?.getMessages,
-            //     //     getMessages: [
-            //     //         ...messageData?.getMessages,
-            //     //         newMessage,
-            //     //     ]
-            //     // }
-            // })
-        }
-    })
-    // const { data: LOL } = useSubscription(NEW_STORE, {
-    //     pollInterval: 10,
-    //     onSubscriptionComplete: () => {
-    //         // const dataMessage = client.readQuery({ query: GET_MESSAGES })
-    //     },
-    //     onSubscriptionData: ({ subscriptionData }) => {
-    //         // client.writeQuery({
-    //         //     query: GET_MESSAGES,
-    //         //     // data: {
-    //         //     //     ...messageData?.getMessages,
-    //         //     //     getMessages: [
-    //         //     //         ...messageData?.getMessages,
-    //         //     //         newMessage,
-    //         //     //     ]
-    //         //     // }
-    //         // })
-    //     }
-    // })
-    const [dataMessage, setDataMessage] = useState([])
-    useEffect(() => {
-        if (messageError) console.log(messageError)
-        messageData?.getMessages && setDataMessage([...messageData?.getMessages])
-        if (messageDataNew) {
-            setDataMessage([...dataMessage, messageDataNew?.newMessage])
-        }
-    }, [messageError, messageDataNew, messageData])
-    // console.log(messageDataNew)
-    useEffect(() => {
-        refetch
-    }, [refetch])
-    const [sendMessage, { loading }] = useMutation(SEND_MESSAGES, {
-        context: { clientName: "admin-server" },
-        // fetchPolicy: 'cache-and-network',
-    })
-    //EFECTOS
-    useEffect(() => {
-        if (selectedStore) {
-            getMessages({ variables: { from: selectedStore?.getOneStore?.idStore } })
-        }
-    }, [selectedStore])
-
-
-    // HANDLESS
-    const input = useRef('')
-    const messagesEndRef = useRef('')
-    const { content } = values || {}
-    useEffect(() => {
-        var objDiv = document.getElementById('scroll');
-        if (messagesEndRef && objDiv) {
-            console.log(messagesEndRef.current)
-            messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
-            objDiv.scrollTop = objDiv.scrollHeight
-        }
-    }, [messageData])
-
-    const handleSendMessage = async e => {
-        e.preventDefault()
-        input.current.focus()
-        input.current.value = ''
-        try {
-            if (selectedStore && id && !!content) {
-                sendMessage({
-                    variables: { to: id, content: content }
-                }).catch(res => {
-                    // input.current.value = ''
-                }).catch(err => setAlertBox({ message: `${err}`, duration: 7000 }))
-            }
-            setValues({})
-        } catch (error) {
-            setAlertBox({
-                message: error.message,
-                duration: 10000,
-                color: 'warning'
-            })
-        }
-
+  })
+  const { data: messageDataNew, error: messageError } = useSubscription(NEW_MESSAGE, {
+    pollInterval: 10,
+    onSubscriptionComplete: () => {
+      // const dataMessage = client.readQuery({ query: GET_MESSAGES })
+    },
+    onSubscriptionData: ({ subscriptionData }) => {
+      // client.writeQuery({
+      //     query: GET_MESSAGES,
+      //     // data: {
+      //     //     ...messageData?.getMessages,
+      //     //     getMessages: [
+      //     //         ...messageData?.getMessages,
+      //     //         newMessage,
+      //     //     ]
+      //     // }
+      // })
     }
-    // subscription's mensajes
+  })
+  // const { data: LOL } = useSubscription(NEW_STORE, {
+  //     pollInterval: 10,
+  //     onSubscriptionComplete: () => {
+  //         // const dataMessage = client.readQuery({ query: GET_MESSAGES })
+  //     },
+  //     onSubscriptionData: ({ subscriptionData }) => {
+  //         // client.writeQuery({
+  //         //     query: GET_MESSAGES,
+  //         //     // data: {
+  //         //     //     ...messageData?.getMessages,
+  //         //     //     getMessages: [
+  //         //     //         ...messageData?.getMessages,
+  //         //     //         newMessage,
+  //         //     //     ]
+  //         //     // }
+  //         // })
+  //     }
+  // })
+  const [dataMessage, setDataMessage] = useState([])
+  useEffect(() => {
+    if (messageError) return {}
+    messageData?.getMessages && setDataMessage([...messageData?.getMessages])
+    if (messageDataNew) {
+      setDataMessage([...dataMessage, messageDataNew?.newMessage])
+    }
+  }, [messageError, messageDataNew, messageData])
+  const [sendMessage, { loading }] = useMutation(SEND_MESSAGES, {
+    context: { clientName: 'admin-server' }
+    // fetchPolicy: 'cache-and-network',
+  })
+  // EFECTOS
+  useEffect(() => {
+    if (selectedStore) {
+      getMessages({ variables: { from: selectedStore?.getOneStore?.idStore } })
+    }
+  }, [selectedStore])
 
 
-    return (
-        <ContainerContextMessage>
-            <MessageComp
-                search={search}
-                messageData={dataMessage || []}
-                setStoreChatActive={setStoreChatActive}
-                data={dataStoreActiveOrder?.getAllStoreActiveChat || []}
-                messagesEndRef={messagesEndRef}
-                selectedStore={selectedStore}
-                input={input}
-                hidden={hidden}
-                id={id}
-                loading={loading}
-                // OneUser={OneUser?.getUser}
-                values={values}
-                handleSendMessage={handleSendMessage}
-                handleChangeFilter={handleChangeFilter}
-                handleChange={handleChange}
-                show={show}
-                setShow={setShow}
-            />
-        </ContainerContextMessage>
-    )
+  // HANDLESS
+  const input = useRef('')
+  const messagesEndRef = useRef('')
+  const { content } = values || {}
+  useEffect(() => {
+    const objDiv = document.getElementById('scroll')
+    if (messagesEndRef && objDiv) {
+      messagesEndRef.current.scrollIntoView({ behavior: 'smooth' })
+      objDiv.scrollTop = objDiv.scrollHeight
+    }
+  }, [messageData])
+
+  const handleSendMessage = async e => {
+    e.preventDefault()
+    input.current.focus()
+    input.current.value = ''
+    try {
+      if (selectedStore && id && !!content) {
+        sendMessage({
+          variables: { to: id, content }
+        }).catch(res => {
+          // input.current.value = ''
+        }).catch(err => { return setAlertBox({ message: `${err}`, duration: 7000 }) })
+      }
+      setValues({})
+    } catch (error) {
+      setAlertBox({
+        message: error.message,
+        duration: 10000,
+        color: 'warning'
+      })
+    }
+  }
+  // subscription's mensajes
+
+
+  return (
+    <ContainerContextMessage>
+      <MessageComp
+        data={dataStoreActiveOrder?.getAllStoreActiveChat || []}
+        handleChange={handleChange}
+        handleChangeFilter={handleChangeFilter}
+        handleSendMessage={handleSendMessage}
+        hidden={hidden}
+        id={id}
+        input={input}
+        loading={loading}
+        messageData={dataMessage || []}
+        messagesEndRef={messagesEndRef}
+        // OneUser={OneUser?.getUser}
+        search={search}
+        selectedStore={selectedStore}
+        setShow={setShow}
+        setStoreChatActive={setStoreChatActive}
+        show={show}
+        values={values}
+      />
+    </ContainerContextMessage>
+  )
 }
--- container/profile/index.jsx
+++ container/profile/index.jsx
@@ -1,45 +1,46 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { BGColor, PColor } from '../../public/colors';
-import { IconCancel, IconTicker, IconLogo, IconSearch } from '../../public/icons';
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { GET_USER, GET_USER_PROFILE, SET_USER_PROFILE } from './queries';
-import { useMutation, useQuery } from '@apollo/client';
-import InputHooks from '../../components/InputHooks/InputHooks';
-import styled, { css, keyframes } from 'styled-components';
-import { filterKeyObject, validationSubmitHooks } from '../../utils';
-import { Context } from '../../context';
-import { RippleButton } from '../../components/Ripple';
+/* eslint-disable camelcase */
+import React, {
+  useContext,
+  useEffect,
+  useState
+} from 'react'
+import { GET_USER, GET_USER_PROFILE } from './queries'
+import { useQuery } from '@apollo/client'
+import InputHooks from '../../components/InputHooks/InputHooks'
+import { validationSubmitHooks } from '../../utils'
+import { Context } from '../../context'
+import { RippleButton } from '../../components/Ripple'
 import { LoadEllipsis } from '../../components/LoadingButton'
-import { Loading } from '../../components/Loading';
-import { Section, Container, Content, AnimationLeft, AnimationRight, BoxInput, Title, ContainerAnimation, ContainerAnimationTow, Input, LabelInput, Paragraph } from './styled';
+import { Loading } from '../../components/Loading'
+import {
+  Section,
+  Container,
+  Content,
+  BoxInput,
+  Title,
+  ContainerAnimation,
+  ContainerAnimationTow,
+  Input,
+  LabelInput,
+  Paragraph
+} from './styled'
 
 
 export const Profile = () => {
-  const { data, loading, error } = useQuery(GET_USER)
+  const { data, loading } = useQuery(GET_USER)
   const { data: dataUp } = useQuery(GET_USER_PROFILE)
-  const [setUserProfile, { loading: loadUP }] = useMutation(SET_USER_PROFILE, {
-    onCompleted: (data) => setAlertBox({ message: 'Datos de usuario cambiados con éxito', color: 'success' })
-
-  })
-
   const { email } = data?.getUser || {}
   const { getOneUserProfile } = dataUp || {}
 
   const { setAlertBox } = useContext(Context)
   const [values, setValues] = useState({})
   const [errors, setErrors] = useState({})
-  // const [UpdateUser, { loading: loadingC }] = useMutation(CHANGE_DATA_USER)
-  const [active, setActive] = useState(1)
+  const [active] = useState(1)
 
-  const handleClick = index => {
-    setActive(index === active ? true : index)
-  }
   const handleChange = (e, error) => {
     setValues({ ...values, [e.target.name]: e.target.value })
     setErrors({ ...errors, [e.target.name]: error })
   }
-  const { currentPassword, newPassword } = values
   const handleSavePass = async e => {
     e.preventDefault()
     // Declarando variables
@@ -55,23 +56,7 @@ export const Profile = () => {
     setErrors({ ...errorForm })
     try {
       if (!errorSubmit) {
-        const response = await setUserProfile({
-          variables: {
-            data: {
-              ...filterKeyObject(values, ['__typename', 'id', 'name', 'lastName', 'email', 'getUser', 'username', 'ULocation', 'avatar', 'createAt', 'description', 'password', 'role', 'siteWeb', 'uPhoNum', 'uState', 'uToken', 'upIdeDoc', 'upLat', 'upLon']),
-              upImage: 'lol',
-              upPhone: values.upPhone,
-              user: {
-                id: values.id,
-                username: values.username,
-                lastName: values.lastName,
-                name: values.name,
-                uAvatar: values.uAvatar,
-                email: values.email
-              }
-            }
-          }
-        })
+        return
       }
     } catch (error) {
       setAlertBox({ message: `${error}` })
@@ -79,67 +64,72 @@ export const Profile = () => {
   }
 
   useEffect(() => {
-    let obj = { ...getOneUserProfile, ...data?.getUser }
+    const obj = { ...getOneUserProfile, ...data?.getUser }
     setValues({ ...obj })
   }, [data, getOneUserProfile, dataUp])
   // console.log(email)
   const [emailFormat, setEmailFormat] = useState('')
-  const [lastEmailFormat, setlastEmailFormat] = useState('')
+  // eslint-disable-next-line no-unused-vars
+  const [_, setlastEmailFormat] = useState('')
   useEffect(() => {
     if (email) {
       const email_regex = email
-      var aa = email_regex?.split("@")
+      const aa = email_regex?.split('@')
       const format = aa[0]
       const formatLength = format.length
-        setEmailFormat(format)
-      // 
-      var lastFormatted = format
-      var last = lastFormatted.substring(formatLength - 4)
+      setEmailFormat(format)
+      //
+      const lastFormatted = format
+      const last = lastFormatted.substring(formatLength - 4)
       setlastEmailFormat(last)
     }
-  }, [data])
+  }, [data, email])
 
   return (
     <Section bg={emailFormat?.repeat(100)}>
-    {/* // <Section bg={emailFormat?.repeat(8)} bg2={"inao jesusjuvinao jesusjuvinao jesusjuvinao jesusjuvinao jesusjuvinao jesusjuvinao jesusjuvinao jesusjuv"} bg3={"sjuvinao jesusjuvinao jesusjuvinao jesusjuvinao jesusjuvinao jesusjuvinao jesusjuvinao jesusjuvinao jesu"}> */}
+      {/* // <Section bg={emailFormat?.repeat(8)} bg2={"inao jesusjuvinao jesusjuvinao jesusjuvinao jesusjuvinao jesusjuvinao jesusjuvinao jesusjuvinao jesusjuv"} bg3={"sjuvinao jesusjuvinao jesusjuvinao jesusjuvinao jesusjuvinao jesusjuvinao jesusjuvinao jesusjuvinao jesu"}> */}
       <Container >
         <Content>
           {loading && <Loading />}
           <Title>Editar datos</Title>
-          <Paragraph>Hola {`${email ? email : 'Bienvenido'}`} </Paragraph>
+          <Paragraph>Hola {`${email || 'Bienvenido'}`} </Paragraph>
           {/* <RippleButton padding='5px' widthButton='45%' onClick={() => active !== 1 && handleClick(1)}>1</RippleButton> */}
           {/* <RippleButton padding='5px' widthButton='45%' onClick={() => active !== 2 && handleClick(2)}>2</RippleButton> */}
           <form onSubmit={handleSavePass} >
-            <React.Fragment> {active === 1 ?
-              <ContainerAnimation>
+            <React.Fragment> {active === 1
+              ? <ContainerAnimation>
                 <BoxInput>
-                  <Input name="name"
-                    value={values.name}
+                  <Input
+                    name='name'
                     onChange={handleChange}
+                    value={values.name}
                   />
                   <LabelInput >{'Nombre de usuario'}</LabelInput>
                 </BoxInput>
                 {<BoxInput>
                   <Input
-                    name="upPhone"
-                    value={values.upPhone}
+                    name='upPhone'
                     onChange={handleChange}
+                    value={values.upPhone}
                   />
                   <LabelInput >{'Teléfono'}</LabelInput>
                 </BoxInput>
                 }
               </ContainerAnimation>
-              : active === 12212 ? <ContainerAnimationTow>
-                <InputHooks name="password"
-                  value={values.password}
-                  errors={errors.password}
-                  onChange={handleChange}
-                  title="Contraseña"
-                  required
-                  type="password"
-                  range={{ min: 0, max: 180 }}
-                />
-              </ContainerAnimationTow> : null}</React.Fragment>
+              : active === 12212
+                ? <ContainerAnimationTow>
+                  <InputHooks
+                    errors={errors.password}
+                    name='password'
+                    onChange={handleChange}
+                    range={{ min: 0, max: 180 }}
+                    required
+                    title='Contraseña'
+                    type='password'
+                    value={values.password}
+                  />
+                </ContainerAnimationTow>
+                : null}</React.Fragment>
             <RippleButton widthButton='100%'>
               {loading ? <LoadEllipsis color='#fff' /> : 'Confirmar'}
             </RippleButton>
@@ -148,4 +138,4 @@ export const Profile = () => {
       </Container>
     </Section>
   )
-};
+}
